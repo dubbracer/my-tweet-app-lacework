c46c05d2a022eab111bada31827d7fcca17bc65c
dubbracer <34395529+dubbracer@users.noreply.github.com>
2022-02-21 16:07:45 +0100

    Update app.py
    resolved typo

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import platform
 app = Flask(__name__)
 
 event_text = "Welcome to our new DevSecOps automation Demo Site Feb 8th 2022"
-tweet_text = "I just just enjoyed a fantastic demonstration about shifting left Security with #laceworks"
+tweet_text = "I just enjoyed a fantastic demonstration about shifting left Security with #laceworks"
 
 
 @app.route('/')
