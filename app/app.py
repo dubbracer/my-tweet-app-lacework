bb9d9898e61268b269aa1947760687d64feff85a
dubbracer <34395529+dubbracer@users.noreply.github.com>
2022-03-24 17:58:26 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import platform
 
 app = Flask(__name__)
 
-event_text = "Welcome :) to our new DevSecOps automation Demo Site March 2022"
+event_text = "Welcome :-) to our new DevSecOps automation Demo Site March 2022"
 tweet_text = "I just enjoyed a fantastic demonstration about shifting left Security with #laceworks"
 
 
