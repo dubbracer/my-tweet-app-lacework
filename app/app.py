54b61490afe5d22e2efa180606efb74327e71f25
dubbracer <34395529+dubbracer@users.noreply.github.com>
2024-09-16 11:09:09 +0200

    Update app.py
    date change in app.py

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 app_name = 'Demo Application for events'
 
-event_text = "Welcome, to our new DevSecOps automation Demo Site in March 2023"
+event_text = "Welcome, to our new DevSecOps automation Demo Site in April 2023"
 tweet_text = "I just enjoyed a fantastic Lacework Sparktour Event about shifting left Security with #lacework"
 
 
